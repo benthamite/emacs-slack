0ba62616a743f9a158e781b4f833257ef759d837
Pablo Stafforini <pablo@stafforini.com>
2026-03-25 10:57:09 -0300

    replace ignore-errors with condition-case + logging
    Replace silent ignore-errors in four locations with condition-case
    that logs errors via message, making hidden bugs visible:
    
    - slack-activity-feed-buffer.el: room name lookup (both instances)
    - slack-message-formatter.el: file summary
    - slack-block.el: confirmation dialog creation
    - slack-message-reaction.el: reaction at point lookup

--- slack-message-formatter.el
+++ slack-message-formatter.el
@@ -175,8 +175,11 @@
 (cl-defmethod slack-message-to-string ((this slack-file) ts team)
   (if (slack-file-hidden-by-limit-p this)
       (slack-file-hidden-by-limit-message this)
-    (let ((body (or (ignore-errors
-                      (slack-file-summary this ts team))
+    (let ((body (or (condition-case err
+                        (slack-file-summary this ts team)
+                      (error
+                       (message "slack-message: file summary error: %S" err)
+                       nil))
                     "<slack-file-summary error>"))
           (thumb (slack-image-string (slack-file-thumb-image-spec this))))
       (slack-format-message
