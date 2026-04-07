f7c224626d93aa84371368ba85eda53004d1d3ff
Pablo Stafforini <pablo@stafforini.com>
2026-04-07 10:04:45 -0300

    Add .dir-locals.el for consistent project settings

---
+++ .dir-locals.el
@@ -0,0 +1,7 @@
+;;; Directory Local Variables            -*- no-byte-compile: t; -*-
+
+((emacs-lisp-mode
+  (indent-tabs-mode . nil)
+  (fill-column . 80)
+  (sentence-end-double-space . nil)
+  (checkdoc-verb-check-experimental-flag . nil)))
