34191b1d5a9f613edc4cae5d3748cfe2077590b4
Pablo Stafforini <pablo@stafforini.com>
2026-04-08 11:58:08 -0300

    Revise transient menu: better grouping and new commands
    Reorganize sections:
    - Move activity feed, saved items, scheduled messages to Navigate
      (they are views you navigate to)
    - Move compose-in-buffer, yank-code-block to new Compose section
      with insert-emoji, embed-mention, embed-channel
    - Move open-in-browser, kill-all-buffers to Team & connection
    - Rename React & pin to React & save; add save-for-later
    - Eliminate the Misc catch-all section
    
    Add missing commands:
    - thread-toggle-subscription (follow/unfollow)
    - message-redisplay (refresh rendering at point)
    - message-save-for-later
    - insert-emoji, embed-mention, embed-channel
    - clipboard-image-upload
    - join-huddle

--- slack-menu.el
+++ slack-menu.el
@@ -32,51 +32,59 @@
     ("m" "direct message" slack-im-select)
     ("u" "rooms" slack-select-rooms)
     ("U" "unread rooms" slack-select-unread-rooms)
-    ("a" "all threads" slack-all-threads)]
+    ("a" "all threads" slack-all-threads)
+    ("A" "activity feed" slack-activity-feed-show)
+    ("*" "saved items" slack-saved-items)
+    ("M" "scheduled messages" slack-scheduled-messages-show)]
    ["Message"
-    ("d" "thread" slack-thread-show-or-create)
-    ("Z" "thread reply" slack-thread-reply)
+    ("d" "open thread" slack-thread-show-or-create)
+    ("Z" "reply in thread" slack-thread-reply)
+    ("W" "thread subscribe" slack-thread-toggle-subscription)
     ("e" "edit" slack-message-edit)
     ("D" "delete" slack-message-delete)
-    ("z" "compose in buffer" slack-message-write-another-buffer)
     ("s" "share" slack-message-share)
     ("l" "copy link" slack-message-copy-link)
     ("I" "copy id" slack-message-copy-id)
-    ("Q" "quote & reply" slack-quote-and-reply)]
-   ["React & pin"
+    ("Q" "quote & reply" slack-quote-and-reply)
+    ("." "redisplay" slack-message-redisplay)]
+   ["React & save"
     ("r" "add reaction" slack-message-add-reaction)
     ("R" "remove reaction" slack-message-remove-reaction)
     ("p" "pin" slack-message-pins-add)
     ("P" "unpin" slack-message-pins-remove)
-    ("n" "pins list" slack-room-pins-list)]
+    ("n" "pins list" slack-room-pins-list)
+    ("B" "save for later" slack-message-save-for-later)]
+   ["Search"
+    ("/" "messages" slack-search-from-messages)
+    ("\\" "files" slack-search-from-files)]]
+  [["Compose"
+    ("z" "compose in buffer" slack-message-write-another-buffer)
+    ("y" "yank code block" slack-yank-code-block)
+    ("E" "insert emoji" slack-insert-emoji)
+    ("@" "embed mention" slack-message-embed-mention)
+    ("#" "embed channel" slack-message-embed-channel)]
    ["File"
     ("f" "upload" slack-file-upload)
     ("F" "quick upload" slack-file-upload-quick)
     ("S" "upload snippet" slack-file-upload-snippet)
-    ("x" "download at point" slack-download-file-at-point)]]
-  [["Search"
-    ("/" "messages" slack-search-from-messages)
-    ("\\" "files" slack-search-from-files)]
+    ("x" "download at point" slack-download-file-at-point)
+    ("X" "clipboard image" slack-clipboard-image-upload)]
    ["Channel"
     ("C" "create" slack-create-channel)
     ("j" "join" slack-channel-join)
     ("v" "leave" slack-channel-leave)
     ("t" "set topic" slack-channel-set-topic)
     ("i" "info" slack-show-channel-info)
-    ("b" "bookmarks" slack-show-channel-bookmarks)]
+    ("b" "bookmarks" slack-show-channel-bookmarks)
+    ("h" "huddle" slack-join-huddle)]
    ["Team & connection"
     ("T" "change team" slack-change-current-team)
     ("o" "start" slack-start-and-select)
     ("O" "stop" slack-disconnect-all)
     ("+" "set status" slack-user-set-status)
-    ("-" "reset status" slack-user-reset-status)]
-   ["Misc"
-    ("*" "saved items" slack-saved-items)
-    ("A" "activity feed" slack-activity-feed-show)
-    ("M" "scheduled messages" slack-scheduled-messages-show)
-    ("K" "kill all buffers" slack-kill-all-buffers)
+    ("-" "reset status" slack-user-reset-status)
     ("w" "open in browser" slack-jump-to-browser)
-    ("y" "yank code block" slack-yank-code-block)]])
+    ("K" "kill all buffers" slack-kill-all-buffers)]])
 
 (provide 'slack-menu)
 ;;; slack-menu.el ends here
